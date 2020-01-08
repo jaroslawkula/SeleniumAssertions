4d7117dd29006da699f0f7b7eb0687bb3ff61113
Jaroslaw Kula <jaroslaw.kula78@gmail.com>
2020-01-08 20:48:49 +0100

    refactoring class name

--- seleniumassertions/src/main/java/assertions/WebElementSoftAssertions.java
+++ seleniumassertions/src/main/java/assertions/WebElementSoftAssertions.java
@@ -3,7 +3,7 @@ package assertions;
 import org.assertj.core.api.AutoCloseableSoftAssertions;
 import org.openqa.selenium.WebElement;
 
-public class AutoCloseableWebElementSoftAssertions extends AutoCloseableSoftAssertions {
+public class WebElementSoftAssertions extends AutoCloseableSoftAssertions {
     public WebElementAssert assertThat(WebElement actual){
         return proxy(WebElementAssert.class, WebElement.class, actual);
     }
